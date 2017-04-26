a8d1988d12d99771384c074ab8a47400348b5e28
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-04-26 16:01:04 +0100

    c

--- ex22a.m
+++ ex22a.m
@@ -2,17 +2,17 @@ lambda = 1;
 invmiu = 90;
 C = 10;
 M = 2;
+N = 1000;
+R = 100000;
 
-R = 10000;
-
-No = 100; %número de simulações
+No = 10; %número de simulações
 results= zeros(1,No); %vetor com os N resultados de simulação
 for it= 1:No
 results(it)= simu1(lambda,invmiu,C,M,R,N);
 end
 
 alfa= 0.1; %intervalo de confiança a 90%
-media = (results);
+media = mean(results);
 termo = norminv(1-alfa/2)*sqrt(var(results)/No);
 fprintf('resultado = %.2e +- %.2e\n',media,termo)
 
